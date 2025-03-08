d5b2a7763c6cee78a52456ebeba1f9d358a3e6cd
mikoCalderon011 <mikocalderon123@gmail.com>
2025-03-08 08:51:46 -0800

    Commit: forgor to remove env as usual

--- models/buildings.model.js
+++ models/buildings.model.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
+const AutoIncrement = require("mongoose-sequence")(mongoose);
 const navigationPathSchema = require("./navigationPath.model");
 const navigationGuideSchema = require("./navigationGuide.model");
 const imageSchema = require("./image.model");
 const roomSchema = require("./room.model");
 
 const buildingSchema = new mongoose.Schema({
-   id: { type: Number, required: true },
    name: { type: String, required: true },
    description: { type: String, required: false },
    path: { type: String, required: true },
@@ -15,5 +15,7 @@ const buildingSchema = new mongoose.Schema({
    image: [imageSchema],
    navigationGuide: [navigationGuideSchema],
 });
+
+buildingSchema.plugin(AutoIncrement, { inc_field: "id"})
 
 module.exports = mongoose.model("Building", buildingSchema);
